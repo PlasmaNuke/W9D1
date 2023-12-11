43af9e802fb55dafe64fa713ff39bc59d9ba41bb
Peter Nolan <peter.l.nolan@gmail.com>
2023-12-11 15:40:27 -0500

    Install NPM dependencies and define basic MovingOption constructor

---
+++ src/moving_object.js
@@ -0,0 +1,10 @@
+
+
+function MovingObject(optionsObj) {
+    this.pos = optionsObj.pos;
+    this.vel = optionsObj.vel;
+    this.radius = optionsObj.radius;
+    this.color = optionsObj.color;
+}
+
+module.exports = MovingObject;
